4806e22cadf75747cb76fc2e530ed81e8ebb9356
woong99 <dndry1072@naver.com>
2022-02-28 22:48:26 +0900

    todo-app

---
+++ todo-app/src/App.js
@@ -0,0 +1,12 @@
+import TodoInsert from './TodoInsert';
+import TodoTemplate from './TodoTemplate';
+
+function App() {
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
+}
+
+export default App;
