5fc2088dd32988c7cdb88fa959c8e77f0e3d8348
woong99 <dndry1072@naver.com>
2022-03-10 21:19:37 +0900

    react-hooks-example

---
+++ react-hooks-example/src/components/Example3.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Example3() {
+  const [state, setState] = React.useState({ count: 0 });
+  return (
+    <div>
+      <p>You clicked {state.count} times</p>
+      <button onClick={click}>Click me</button>
+    </div>
+  );
+
+  function click() {
+    setState((state) => ({ count: state.count + 1 }));
+  }
+}
