a3b3e9d99c6e82bce13f0c14dd38485db5aaed3b
woong99 <dndry1072@naver.com>
2022-03-18 14:21:46 +0900

    redux-start

---
+++ redux-start/src/redux/reducers/reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import todos from './todos';
+import filter from './filter';
+
+const reducer = combineReducers({
+  todos: todos,
+  filter: filter,
+});
+
+export default reducer;
