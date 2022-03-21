318acf60b11b65a3765e52b2f5d564e065931bd5
woong99 <dndry1072@naver.com>
2022-03-21 13:30:33 +0900

    redux-study

--- redux-study/src/redux/store.js
+++ redux-study/src/redux/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import todoApp from './reducers/reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
 
 // function middleware1(store) {
 //   console.log('middleware1', 0);
@@ -28,6 +29,6 @@ import thunk from 'redux-thunk';
 //     };
 //   };
 // }
-const store = createStore(todoApp, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(todoApp, composeWithDevTools(applyMiddleware(thunk, promise)));
 
 export default store;
