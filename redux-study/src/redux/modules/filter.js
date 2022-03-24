a015e5455f3e4f500473a463260aab338e9d4aa5
woong99 <dndry1072@naver.com>
2022-03-24 20:03:50 +0900

    redux-study

--- redux-study/src/redux/modules/filter.js
+++ redux-study/src/redux/modules/filter.js
@@ -1,26 +1,17 @@
-// 액션 타입 정의
-const SHOW_ALL = 'redux-study/filter/SHOW_ALL';
-const SHOW_COMPLETE = 'redux-study/filter/SHOW_COMPLETE';
+import { createActions, handleActions } from 'redux-actions';
 
-// 액션 생성 함수
-export function showAll() {
-  return { type: SHOW_ALL };
-}
-export function showComplete() {
-  return { type: SHOW_COMPLETE };
-}
+export const { showAll, showComplete } = createActions('SHOW_ALL', 'SHOW_COMPLETE', {
+  prefix: 'redux-study/filter',
+});
 
 // 초기값
 const initialState = 'ALL';
 
-// 리듀서
-export default function reducer(previousState = initialState, action) {
-  if (action.type === SHOW_COMPLETE) {
-    return 'COMPLETE';
-  }
+const reducer = handleActions(
+  { SHOW_ALL: (state, action) => 'ALL', SHOW_COMPLETE: () => 'COMPLETE' },
+  initialState,
+  { prefix: 'redux-study/filter' },
+);
 
-  if (action.type === SHOW_ALL) {
-    return 'ALL';
-  }
-  return previousState;
-}
+// 리듀서
+export default reducer;
